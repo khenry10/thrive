8f872015f04e80d260607e5fb14d328d60df7377
Keith Prifte <kprift@gmail.com>
2016-03-18 17:03:41 -0400

    adds list and task create, but task from isn't working yet

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,4 +8,19 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.create!(list_params)
+
+    redirect_to @list
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name, :list_type, :category, :list_time, :deadline, :deadline)
+  end
+
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,4 +8,20 @@ class TasksController < ApplicationController
     task = Task.find(params[:id])
   end
 
+  def new
+    task = Task.new
+  end
+
+  def create
+    task = Task.create!(task_param)
+
+    redirct_to @list
+  end
+
+  private
+  def task_param
+    params.require(:task).permit(:name, :category, :priority, :description, :time_estimate, :time_actual, :costs, :deadline, :image, :quantity, :list_id)
+  end
+
+
 end
