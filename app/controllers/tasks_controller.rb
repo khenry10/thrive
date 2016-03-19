fd28dd9d8703749acb32db7c56d08fbbf041a4cd
Keith Prifte <kprift@gmail.com>
2016-03-19 14:53:26 -0400

    adds task edit ability

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,11 @@ class TasksController < ApplicationController
     redirect_to list_path(@list)
   end
 
+  def edit
+    @list = List.find(params[:list_id])
+    @task = Task.find(params[:id])
+  end
+
   private
   def task_param
     params.require(:task).permit(:name, :category, :priority, :description, :time_estimate, :time_actual, :costs, :deadline, :image, :quantity, :list_id)
